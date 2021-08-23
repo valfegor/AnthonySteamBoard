b5b8020848634d652b6ec24b225ccbc2905ccc13
Anthony Ahumada <locopala99@gmail.com>
2021-08-23 19:10:00 +0200

    More js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,23 @@
 const User = require('../models/user');
 const Role = require('../models/role');
 const mongoose = require('mongoose');
-const bcrypt = require('brcrypt');
+const bcrypt = require("bcrypt");
 
 
 const registerNormalUser = async (req, res) => {
     if(!req.body.name || !req.body.password || !req.body.email) return res.status(400).send("Sorry please check te camps");
 
-    const existingEmail = await User.find({email:req.user.email});
+    const existingEmail = await User.findOne({email:req.body.email})
 
     if(existingEmail) return res.status(400).send("The email its already created please checkout");
 
-    const role = Role.findOne({name:"user"});
+    const role = await Role.findOne({name:"user"});
 
     if(!role) return res.status(400).send("Sorry no role asigned");
 
-    const hash = bcrypt.hash(req.body.password,10);
+    const hash = await bcrypt.hash(req.body.password,10);
 
+    console.log(role._id);
 
     let user = new User({
         name: req.body.name,
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const {Database_connect} = require('../backend/bd/bd');
-const Role = require('../backend/routes/roleRoutes')
+const Role = require('../backend/routes/roleRoutes');
+const User = require('../backend/routes/userRoutes');
 
 app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use('/api/roles',Role);
+app.use('/api/user',User)
 
 app.listen(process.env.PORT,()=>console.log("Backend Server Running  Ok On Port",process.env.PORT))
 
