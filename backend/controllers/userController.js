18df0eebe8af51150fadae9539c820f0c6bf081c
Anthony Ahumada <locopala99@gmail.com>
2021-08-23 18:57:59 +0200

    More js

---
+++ backend/controllers/userController.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+const Role = require('../models/role');
+const mongoose = require('mongoose');
+const bcrypt = require('brcrypt');
+
+
+const registerNormalUser = async (req, res) => {
+    if(!req.body.name || !req.body.password || !req.body.email) return res.status(400).send("Sorry please check te camps");
+
+    const existingEmail = await User.find({email:req.user.email});
+
+    if(existingEmail) return res.status(400).send("The email its already created please checkout");
+
+    const role = Role.findOne({name:"user"});
+
+    if(!role) return res.status(400).send("Sorry no role asigned");
+
+    const hash = bcrypt.hash(req.body.password,10);
+
+
+    let user = new User({
+        name: req.body.name,
+        password:hash,
+        email:req.body.email,
+        id_Role:role._id,
+        Status:true
+    });
+
+    let result = await user.save();
+
+    if(!result) return res.status(400).send("Sorry Please try again");
+
+    try {
+        let jwtToken = user.generateJWT();
+        return res.status(200).send({jwtToken});
+    } catch (e) {
+        return res.status(400).send("Sorry please try again");
+    }
+
+}
+
+
+
+
+
+module.exports = {registerNormalUser}
