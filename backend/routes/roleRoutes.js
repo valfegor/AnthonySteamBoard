2474ec0dbae2fb8ae03395bd7b4166f75d068453
Anthony Ahumada <locopala99@gmail.com>
2021-08-24 18:39:33 +0200

    more js

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
+const Auth = require('../middlewares/Auth');
+
+const Validate = require('../middlewares/validate');
+
+const Admin = require('../middlewares/Admin');
+
 const router = express.Router();
 
 const roleController = require('../controllers/roleController');
 
-router.post('/registerRole',roleController.registerRole);
+router.post('/registerRole',Auth,Validate,Admin,roleController.registerRole);
 
-router.get('/listRole',roleController.listRoles);
+router.get('/listRole',Auth,Validate,Admin,roleController.listRoles);
 
-router.put('/updateRole',roleController.updateRole);
+router.put('/updateRole',Auth,Validate,Admin,roleController.updateRole);
 
 
 module.exports = router;
