5d6575addb1af9a5f85baf54370f998f658ab389
Anthony Ahumada <locopala99@gmail.com>
2021-08-24 20:05:10 +0200

    more js

--- backend/controllers/boardController.js
+++ backend/controllers/boardController.js
@@ -28,39 +28,42 @@ const registerTask = async (req, res) => {
 }
 
 //Despues de las validaciones por medio del middleware se ejecuta la funcion
-const registerTaskImg = async (req, res) => {
-    if(!req.body.name || !req.body.description) return res.status(400).send("Sorry please check all the camps please.");
 
-    //la url que vamos a guardar en nuestra base.
-
-    let imageUrl = "";
-
+const saveTaskImg = async (req, res) => {
+    if (!req.body.name || !req.body.description)
+    return res.status(400).send("Incomplete data");
+    //necesitamos generar una URL para cargar la imagen
+    let imageUrl = ""; //http://localhost:3001/
+    console.log(req.files);
+    //si viene la imagen o si viene otro tipo de archivo no soportado
     if(req.files !== undefined && req.files.image.type){
+      //la palabra protocol es la primera parte de la url es decir el protocolo HTTP del request obtenemos el host que llegue
+      let url = req.protocol + "://" + req.get('host')+"/";
+      //aqui va a quedar la imagen en nuestro servidor
+      //queda de la siguiente manera //http:localhost:3001/uploads/57485 -c:user/desktop/js.png (codigo que saquemos por fecha) el path + el nombre de la extension
+      //el path es la ruta de donde se cargo , es decir sacar la extension
+      let serverImg = "./uploads/" + moment().unix() + path.extname(req.files.image.path);
+      //leer se encarga de tomar el archivo , por favor cargue la imagen
+      //Despues de leer se escribe es la carpeta donde va a quedar guardado como se llama y la extension , ya con esta linea queda guardado en la carpeta uploads.
+      fs.createReadStream(req.files.image.path).pipe(fs.createWriteStream(serverImg));
+        //en la bd vamos a guardar un campo.
 
-        let url = req.protocol + "://" + req.get('host')+"/";
-
-        let serverImg = "./uploads" + moment().unix() + path.extname(req.files.path);
-
-        fs.createReadStream(req.files.image.path).pipe(fs.createWriteStream(serverImg));
-
-        imageUrl = url + "uploads/" + moment().unix() + path.extname(req.files.image.path);
-
+      imageUrl = url + "uploads/" + moment().unix() + path.extname(req.files.image.path);
     }
+  let board = new Board({
+    id_user:req.user._id,
+    name: req.body.name,
+    description: req.body.description,
+    taskStatus: "to-do",
+    imageUrl:imageUrl,
+  });
 
-    
-    const board = new Board({
-        name: req.body.name,
-        description: req.body.description,
-        Status:"to-do",
-        imageUrl:imageUrl,
-        id_user:req.user._id,
-    })
+  let result = await board.save();
+  if (!result) return res.status(400).send("Error registering task");
+  return res.status(200).send({ result });
 
-    let result = await board.save();
+  };
 
-    if(!result) return res.status(400).send("Sorry please try again.");
-
-}
 
 const updateTask = async (req, res) => {
 
@@ -108,4 +111,4 @@ const removeTask = async (req, res) => {
 }
 
 
-module.exports = {registerTask,updateTask,listTask,removeTask}
+module.exports = {registerTask,updateTask,listTask,removeTask,saveTaskImg}
--- backend/routes/boardRoutes.js
+++ backend/routes/boardRoutes.js
@@ -8,6 +8,12 @@ const validate = require('../middlewares/validate');
 
 const boardController = require('../controllers/boardController');
 
+const multiparty = require("connect-multiparty");
+
+const multi = multiparty();
+
+const Upload = require('../middlewares/image');
+
 router.post("/registerTask",Auth,validate,boardController.registerTask);
 
 router.put("/modifyTask",Auth,validate,boardController.updateTask);
@@ -15,5 +21,7 @@ router.put("/modifyTask",Auth,validate,boardController.updateTask);
 router.delete("/deleteTask/:_id",Auth,validate,boardController.removeTask);
 
 router.get("/listTask",Auth,validate,boardController.listTask);
+
+router.post("/imageTask",multi,Upload,Auth,validate,boardController.saveTaskImg);
 
 module.exports = router;
