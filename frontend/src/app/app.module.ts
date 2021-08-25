f9c33e11175394093a9fa64cf5806a8f6d521069
Anthony Ahumada <locopala99@gmail.com>
2021-08-25 14:52:13 +0200

    More js

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,14 @@ import { HeaderComponent } from './home/header/header.component';
 import { LoginComponent } from './home/login/login.component';
 import { RegisterComponent } from './home/register/register.component';
 import { FooterComponent } from './home/footer/footer.component';
+import { ListTaskComponent } from './board/list-task/list-task.component';
+import { SaveTaskComponent } from './board/save-task/save-task.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { FooterComponent } from './home/footer/footer.component';
     HeaderComponent,
     LoginComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    ListTaskComponent,
+    SaveTaskComponent,
+    ListUserComponent,
+    RegisterUserComponent,
+    UpdateUserComponent,
+    RegisterRoleComponent,
+    ListRoleComponent,
+    UpdateRoleComponent
   ],
   imports: [
     BrowserModule,
